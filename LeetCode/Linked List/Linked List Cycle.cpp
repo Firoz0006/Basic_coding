31fb097b0413e7487541de0c324cffb4de506605
Firoz0006 <firozalam1160006@gmail.com>
2024-10-29 18:55:07 +0530

    new

--- LeetCode/Linked List/Linked List Cycle.cpp
+++ LeetCode/Linked List/Linked List Cycle.cpp
@@ -14,7 +14,7 @@ class Solution {
     bool hasCycle(ListNode *head) {
         ListNode *slow=head;
         ListNode *fast=head;
-        while(fast!=NULL && fast->next!=NULL){
+        while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
--- LeetCode/Linked List/Remove Nth Node From End of List.cpp
+++ LeetCode/Linked List/Remove Nth Node From End of List.cpp
@@ -4,18 +4,45 @@ using namespace std;
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* res=new ListNode(0,head);
-        ListNode* dummy=res;
-        for(int i=0;i<n;i++)
-        {
-            head=head->next;
+        ListNode* temp = head;
+        int count = 0;
+
+        // Count the number of nodes in the list
+        while (temp) {
+            count++;
+            temp = temp->next;
         }
-        while(head!=nullptr)
-        {
-            head=head->next;
-            dummy = dummy->next;
+
+        int step = count - n;
+
+        // If the node to remove is the head node
+        if (step == 0) return head->next;
+
+        temp = head;
+        
+        // Move to the node just before the one to be removed
+        for (int i = 1; i < step; i++) {
+            temp = temp->next;
         }
-        dummy->next=dummy->next->next;
-        return res->next;
+
+        // Remove the nth node from the end
+        temp->next = temp->next->next;
+
+        return head;
+
+
+        // ListNode* res=new ListNode(0,head);
+        // ListNode* dummy=res;
+        // for(int i=0;i<n;i++)
+        // {
+        //     head=head->next;
+        // }
+        // while(head!=nullptr)
+        // {
+        //     head=head->next;
+        //     dummy = dummy->next;
+        // }
+        // dummy->next=dummy->next->next;
+        // return res->next;
     }
 };
