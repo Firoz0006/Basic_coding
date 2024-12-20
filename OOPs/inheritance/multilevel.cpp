98ab49f29ed8fc550f9a603af28a3aa52322eb23
Firoz0006 <firozalam1160006@gmail.com>
2024-12-20 21:59:27 +0530

    new

--- OOPs/inheritance/multilevel.cpp
+++ OOPs/inheritance/multilevel.cpp
@@ -1,30 +1,37 @@
-#include<bits/stdc++.h>
+
 //Multi Level  inheritance example
+//Defination:-A class can also be derived from one class, which is already derived from another class.
+#include<bits/stdc++.h>
 using namespace std;
-class A
-{
+//Base class(parent class)
+class myclass{
+
 	public:
-	void Afunc()
-	{
-		cout<<"Func A\n";
-	}
-};
-class B: public A
-{
-    public:
-    void Bfunc()
-    {
-        cout<<"Func B\n";
-    }
-};
-class C: public B 
-{
-    public :
+        void myfunction(){
+            cout<<"some content in base class"<<endl;
+        }
 };
 
-int main()
-{
-    C c;
-	c.Afunc();
-	c.Bfunc();
+//Derived class(child class)
+class mychild: public myclass{
+    public:
+        void mychildfunction(){
+            cout<<"some content in child class"<<endl;
+        }
+};
+
+//Derived class(child class)
+class mygrandchild: public mychild{
+    public:
+        void mygrandchildfunction(){
+            cout<<"some content in grandchild class"<<endl;
+        }
+};
+
+int main(){
+    mygrandchild obj;
+    obj.myfunction();
+    obj.mychildfunction();
+    obj.mygrandchildfunction();
+    return 0;
 }
